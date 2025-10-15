d67113ef35591d0315893185fffeffe2faec6cfd
enricodelazzari <enrico.delazzari@maize.io>
2025-10-15 14:41:50 +0200

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,21 +2,11 @@
 
 namespace Maize\GoogleRecaptchaV3\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Maize\GoogleRecaptchaV3\GoogleRecaptchaV3ServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Maize\\GoogleRecaptchaV3\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
@@ -27,11 +17,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-         foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/../database/migrations') as $migration) {
-            (include $migration->getRealPath())->up();
-         }
-         */
     }
 }
