6141e8afa44f53d6ad7027d1f4ad0adc2847a57a
enricodelazzari <enrico.delazzari@maize.io>
2025-10-17 11:43:56 +0200

    FIX config file

--- config/google-recaptcha-v3.php
+++ config/google-recaptcha-v3.php
@@ -1,11 +1,48 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Enabled
+    |--------------------------------------------------------------------------
+    |
+    | This option controls whether Google reCAPTCHA v3 is enabled.
+    | When disabled, the validation will be skipped.
+    |
+    */
     'enabled' => null,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Site Key
+    |--------------------------------------------------------------------------
+    |
+    | Your Google reCAPTCHA v3 site key.
+    | You can get this from https://www.google.com/recaptcha/admin
+    |
+    */
     'site_key' => null,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Secret Key
+    |--------------------------------------------------------------------------
+    |
+    | Your Google reCAPTCHA v3 secret key.
+    | You can get this from https://www.google.com/recaptcha/admin
+    |
+    */
     'secret_key' => null,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Score Threshold
+    |--------------------------------------------------------------------------
+    |
+    | The minimum score threshold for the reCAPTCHA validation.
+    | Google reCAPTCHA v3 returns a score (1.0 is very likely a good interaction,
+    | 0.0 is very likely a bot). Default is 0.5.
+    |
+    */
     'score_threshold' => null,
 ];
